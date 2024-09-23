64a015643d67855ed57efd35b4c803e098610a3d
José Francisco Santos Salazar <josejfss98@gmail.com>
2024-09-23 14:25:55 -0600

    Update app.py

--- Cliente/app.py
+++ Cliente/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.after_request
 def after_request(response):
-    response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Origin"] = "localhost:3000" # <- You can change "*" for a domain for example "http://localhost"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
     response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Bearer"
